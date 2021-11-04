5bf90eabac653a31bc93e3c7f9ce372f08a66c3f
Mihail <admminn@mailru>
2021-11-04 05:44:46 -0700

    lineChartView added

--- MafDataLoader/ViewController.swift
+++ MafDataLoader/ViewController.swift
@@ -8,20 +8,48 @@
 import UIKit
 import CoreBluetooth
 import OSLog
+import Charts
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ChartViewDelegate {
     @IBOutlet weak var mafDataConnectButton: UIButton!
     @IBOutlet weak var mafDataReadButton: UIButton!
     @IBOutlet weak var mafDataWriteButton: UIButton!
-    @IBOutlet weak var mafDataText: UITextView!
+//    @IBOutlet weak var mafDataText: UITextView!
+    @IBOutlet weak var lineChartView: LineChartView!
     
     private var observer: NSObjectProtocol!
     
+    let yValues: [ChartDataEntry] = [
+                    ChartDataEntry(x: 0, y: 1),
+                    ChartDataEntry(x: 1, y: 2),
+                    ChartDataEntry(x: 2, y: 3),
+                    ChartDataEntry(x: 3, y: 4)]
+    
+    var lineChartDataSet: LineChartDataSet!
+    var lineChartData: LineChartData!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //centralManager = CBCentralManager(delegate: self, queue: nil, options: nil)
         // Do any additional setup after loading the view.
+        //lineChartView.backgroundColor = .systemBlue
+        lineChartDataSet = LineChartDataSet(entries: yValues, label: "mafData")
+        
+        lineChartDataSet.lineWidth = 2
+        lineChartDataSet.setColor(.black)
+        lineChartDataSet.highlightColor = .systemRed
+        
+        lineChartData = LineChartData(dataSet: lineChartDataSet)
+        lineChartView.data = lineChartData
+        
+        lineChartView.rightAxis.enabled = false
+        
+        lineChartView.leftAxis.labelFont = .boldSystemFont(ofSize: 12)
+        lineChartView.leftAxis.labelPosition = .outsideChart
+        
+        lineChartView.xAxis.labelPosition = .bottom
+        lineChartView.xAxis.labelFont = .boldSystemFont(ofSize: 12)
+        
+        lineChartView.animate(xAxisDuration: 1.5)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -47,6 +75,7 @@ class ViewController: UIViewController {
         NotificationCenter.default.removeObserver(observer!)
     }
     
+    //MARK: Actions
     @IBAction func mafDataConnect(_ sender: UIButton) {
         if mafDataConnectButton.titleLabel?.text == "Connect" {
             if BleManager.Ble.isPeripheralSelected {
@@ -73,21 +102,27 @@ class ViewController: UIViewController {
         BleManager.Ble.peripheralWrite(string: "Katea jopa")
     }
     
+    //MARK: Line Chart Delegates
+    func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+        print(entry)
+    }
+    
     //MARK: Private Methods
     private func peripheralDataNotifications(notification: Notification) {
         if let data = notification.object as? String {
             switch data {
             case "NewBleData":
+                return
              /*   if let string = String(bytes: BleManager.Ble.peripheralData, encoding: .windowsCP1251) {
                     mafDataText.text = string
                 }*/
-                var dataString: String = ""
+             /*   var dataString: String = ""
                 
                 for dataByte in BleManager.Ble.peripheralData {
                     dataString.append(String(format: "0x%02X ", dataByte))
                 }
                 
-                mafDataText.text = dataString
+                mafDataText.text = dataString */
             case "SelectedBleConnected":
                 mafDataConnectButton.setTitle("Disconnect", for: .normal)
                 mafDataReadButton.isEnabled = true
